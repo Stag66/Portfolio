1ee6044539a448bfa930554b2e686d119fc89776
Kareem Gordon-Graham <32969606+Stag66@users.noreply.github.com>
2023-06-13 16:57:26 -0400

    Add files via upload
    This short probability game highlights efficient coding practices through my use of loops and variables.

---
+++ BeatTheHouse.py
@@ -0,0 +1,21 @@
+import random
+print("Welcome to Beat The House!'. Players will start with $100 and attempt to predict the outcome of of 4 seperate coin tosses in order to profit from the casino. Gain or lose $25 with each outcome. Running out of money will disqualify you; Choose wisely!")
+
+usermoney = 100
+
+for x in range(0,4):
+    user_choice = input("Heads(0) or Tails(1)?")
+    flipresult = random.randint(0, 1)
+    print("Flip result: " + str(flipresult))
+    if user_choice == str(flipresult):
+        print("Nice!")
+        usermoney+=25;
+    else: 
+        print("Wrong! Good Try.")
+        usermoney-=25;
+      
+    if(usermoney<0):
+        print("You ran out of money!Try again.")
+    else: print(usermoney)
+
+print("You finished with $" + str(usermoney) + ". Good Work!")
